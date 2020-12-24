a20118cb9030df8559fbe5f773c6591157c20601
miniqb <1563128533@qq.com>
2020-12-24 18:58:12 +0800

    12/24 控制初步完成

--- src/Judge/ChoiceJudge.java
+++ src/Judge/ChoiceJudge.java
@@ -20,6 +20,10 @@ public class ChoiceJudge extends Judge{
             case C_GO:
                 if(board.GetAimSelect()==board.GetNowSelect()||!is_right_position)
                     result=false;
+            case C_GIVE_UP:
+            case C_RETRACT:
+            default:
+                break;
         }
         if(result) {
             player_now=player_now==player_1?player_2:player_1;
